a6063a71ea724ba2beda10dbb888ce5560abe4e0
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-08-13 17:46:42 +0200

    Aktualisierungen.

--- test.ts
+++ test.ts
@@ -1,10 +1,1 @@
 // tests go here; this will not be compiled when this package is used as a library
-oledssd1306.initDisplay()
-basic.forever(() => {
-    oledssd1306.setTextXY(0, 0)
-    oledssd1306.writeString("123abc")
-    oledssd1306.setTextXY(1, 0)
-    oledssd1306.writeString("ÄÖÜäöüß€°§")
-    oledssd1306.setTextXY(2, 0)
-    oledssd1306.writeString("" + input.temperature() + "°C")
-})
